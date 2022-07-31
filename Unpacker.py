80342c5e2b4e302f352732abb43b7bcf7e5c539f
synch12 <n_synch@hotmail.com>
2022-08-01 08:34:04 +1200

    File Unpacker

---
+++ Unpacker.py
@@ -0,0 +1,54 @@
+import sys
+import struct
+from typing import BinaryIO, Dict
+import math
+
+
+def unpack(in_stream: BinaryIO, out_path):
+    
+    bytesIn = in_stream.read()
+    iterator = 0
+    fileCount = int.from_bytes(bytesIn[0:4],byteorder='little')
+    lengths =  dict()
+    for i in range(fileCount):
+        lengths[i] = int.from_bytes(bytesIn[((i+1)<<2):(((i+1)<<2) +4)],byteorder='little') << 4
+    readOffset = (fileCount+1) * 4
+    readOffset = readOffset + ((0x10 -(readOffset % 0x10))&0xf)
+    iterator = math.ceil(readOffset)
+    print(hex(iterator))
+    for i in range(fileCount):
+        print(i)
+        buffer = bytearray()
+        for j in range(lengths[i]):
+            buffer += bytesIn[readOffset + j].to_bytes(1, byteorder='little')
+        out_stream = open(out_path + str(i) + getExtension(buffer), 'wb')
+        out_stream.write(buffer)
+        out_stream.close()
+        readOffset+=lengths[i]
+        
+
+def getExtension(buffer):
+    extensions = {
+        "TIM2": ".tm2",
+        "GT1G": ".g1t",
+        "MESC": ".strings"
+
+    }
+    return extensions.get(buffer[0:4].decode("utf-8"),'.dat')
+
+
+
+
+
+
+def unpack_file(in_path, out_path):
+    with open(in_path, 'rb') as in_file:
+        unpack(in_file, out_path)
+
+def main():
+    unpack_file(sys.argv[1], sys.argv[2])
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ lzp2Extract.py
@@ -0,0 +1,96 @@
+###
+###  Extracts lzp2 files contents as '.dat' files specific tools are needed to access the .dat
+###
+###
+import Unpacker.py
+import tempfile
+
+import sys
+import struct
+from typing import BinaryIO
+
+def decompress_lzp2(in_stream: BinaryIO, out_path):
+    gap = 0
+    searchvalue = 0x218F
+    file_count = 0
+    Raw = False
+    bytesIn = in_stream.read()
+    datasize = len(bytesIn)
+    iterator = 0x10
+    length = struct.unpack('<I', bytesIn[12:0x10])[0]
+    print(hex(length))
+    buffer = bytearray()
+    #buffer += 0x00.to_bytes(1, byteorder='little')
+    while(length+0x10>iterator):
+        if gap == 0:
+            flag  = bytesIn[iterator] &0XC0
+            gap = bytesIn[iterator] if not flag else 0
+            iterator += 1 if gap >0 else 0
+        for i in range(gap):
+            buffer += bytesIn[iterator].to_bytes(1, byteorder='little')
+            iterator += 1
+        gap, iterator = handlereference(iterator,length,buffer,bytesIn)
+        iterator +=1
+
+    if(not Raw):
+        temp = tempfile.tempfile.TemporaryFile()
+        temp.write(buffer)
+        Unpacker.unpack(temp,out_path)
+    else:
+        out_stream = open(out_path + str(file_count) + '.dat', 'wb')
+        out_stream.write(buffer)
+        out_stream.close()
+
+def handlereference(iterator,length,buffer,bytesIn):
+    #print(iterator,'/', length)
+    gap = 0
+    if(bytesIn[iterator] & 0x80):
+        #print('symbol == ' + hex(bytesIn[iterator]))
+        currentByte = bytesIn[iterator]
+        flag = 3
+        flag += 1 if currentByte & 0x08 else 0
+        flag += 2 if currentByte & 0x10 else 0
+        flag += 4 if currentByte & 0x20 else 0
+        flag += 8 if currentByte & 0x40 else 0
+        iterator +=1
+        Offset = bytesIn[iterator]
+        #print('offset == ' + hex(bytesIn[iterator]))
+        temp =0
+        temp +=bytesIn[iterator - 1]&7
+        Offset += temp<<8
+        Offset += 1
+        if Offset == 1:
+            for i in range(flag):
+                buffer += (0).to_bytes(1, byteorder='little')
+        else:
+            for i in range(flag):
+                buffer += buffer[-Offset:-Offset+1]
+    elif bytesIn[iterator] & 0x40:
+        iterator = RLEread(iterator,length,buffer,bytesIn)
+    else:
+        gap = bytesIn[iterator]
+    return gap, iterator
+
+def RLEread(iterator,length,buffer,bytesIn):
+    iterator += 1
+    temp = bytesIn[iterator-1]&0x0f
+    amount = bytesIn[iterator] + 3 +(temp <<8)
+    iterator += 1
+    value = bytesIn[iterator]
+    for i in range(amount + 1):
+        buffer += value.to_bytes(1, byteorder='little')
+    
+    return  iterator
+
+
+def decompress_lzp2_file(in_path, out_path):
+    with open(in_path, 'rb') as in_file:
+        decompress_lzp2(in_file, out_path)
+
+def main():
+    decompress_lzp2_file(sys.argv[1], sys.argv[2])
+
+
+if __name__ == "__main__":
+    main()
+
