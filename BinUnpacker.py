bc7596715eeaf08533f8a26e01165126285a12dd
synch12 <n_synch@hotmail.com>
2022-08-02 05:38:26 +1200

    Added modified .bin unpacker

---
+++ BinUnpacker.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+
+'''
+Copyright © 2021  Bartłomiej Duda
+License: GPL-3.0 License 
+'''
+
+
+# Program tested on Python 3.7.0
+# It should be used with Dynasty Warriors 8
+
+# Ver    Date        Author               Comment
+# v0.1   28.03.2021  Bartlomiej Duda      -
+
+import os
+import sys
+import struct
+import datetime
+
+
+def bd_logger(in_str):
+    '''
+    Function for logging debug messages
+    '''   
+    now = datetime.datetime.now()
+    print(now.strftime("%d-%m-%Y %H:%M:%S") + " " + in_str)    
+   
+
+def export_data(bin_in_file_path, idx_in_file_path, out_folder_path):
+    '''
+    Function for exporting data from BIN files
+    '''    
+    bd_logger("Starting export_data...")  
+    
+    if not os.path.exists(out_folder_path):
+        os.makedirs(out_folder_path)      
+    
+    bin_file = open(bin_in_file_path, "rb")
+    
+    idx_file = open(idx_in_file_path, "rb")
+    
+    idx_size = os.path.getsize(idx_in_file_path)
+    num_of_idx_entries = int(idx_size / 16) - 1
+    section_start = 0
+    f_count = 0
+    idx_file.read(16)
+    for i in range(num_of_idx_entries):
+        Block1 = struct.unpack("<Q", idx_file.read(8))[0]
+        Block2 = struct.unpack("<Q", idx_file.read(8))[0]
+        section_start = (Block1& 0xffffffff)<<11
+        comp_size = ((Block1&0xffffffff00000000) >>21)
+        selection_end = Block2& 0xffffffff
+        comp_flag = (Block2 >>32) & 0xffffffff
+        # print("Block 2 "+ hex(Block2))
+        # print("Comp size " + hex(comp_size))
+        # print("<< Operation " + hex(temp1))
+        # print("Offset from last = " + hex(temp2))
+        # input()
+        # print("Section Start " + hex(section_start))
+
+
+        if comp_size != 0:
+            f_count += 1            
+            ext = ""
+
+            if comp_flag == 1:
+                ext = ".comp"
+            else:
+                ext = ".dat"
+            bin_file.seek(section_start)
+            ID = struct.unpack("<Q",bin_file.read(8))[0]
+            if ID&0xffffffff == 0x53564F4B:
+                ext = ".kovs"
+            elif ID&0xffffffff == 0x47315447:
+                ext = ".g1t"
+            elif ID&0xffffffff == 0x324D4954:
+                ext = ".tm2"
+            elif ID&0xffffffff == 0x32505A4C:
+                ext = ".lzp"
+            currentstart = section_start
+            f_path = out_folder_path +str(f_count - 1).ljust(9)+hex(section_start) + ext 
+            out_file = open(f_path, 'wb')
+            bin_file.seek(currentstart)
+            while selection_end > 0:
+                if selection_end > 0xffff:
+                    f_data = bin_file.read(0xffff)
+                    out_file.write(f_data)
+                    selection_end -= 0xffff
+                else:
+                    f_data = bin_file.read(selection_end)
+                    out_file.write(f_data)
+                    selection_end = 0
+                #currentstart+=0xffff
+            out_file.close()            
+            f_path = out_folder_path + "file" + str(f_count) + ext 
+            print(f_path)
+   
+    idx_file.close()
+    bin_file.close()
+    bd_logger("Ending export_data...")    
+    
+    
+    
+    
+def main():
+    '''
+    Main function of this program. If you are planning to use it,
+    you should adjust paths first.
+    '''   
+    main_switch = 1
+    # 1 - data export 
+    
+
+    if main_switch == 1:
+        p_bin_in_file_path = "E:\\Emulation\\Emulators\\iso\\dw5\\LINKDATA.BIN"  
+        p_idx_in_file_path = "E:\\Emulation\\Emulators\\iso\\dw5\\LINKDATA.IDX"
+        p_out_folder_path =  "E:\\Emulation\\Emulators\\iso\\dw5\\read\\"
+        export_data(p_bin_in_file_path, p_idx_in_file_path, p_out_folder_path)
+        
+    else:
+        bd_logger("Wrong option selected!")
+        
+        
+    
+    bd_logger("End of main...")    
+    
+    
+    
+main()
+
+# "E:\\Emulation\\Emulators\\iso\\dw5XL\\LINKDATA.BIN"
+# "E:\\Emulation\\Emulators\\iso\\dw5XL\\LINKDATA.IDX"
+# "E:\\Emulation\\Emulators\\iso\\dw5XL\\owow"
+
+
+# "C:\\Users\\Malachi\\WorkDesktop\\Shin Sangokumusou 4 Special\\linkdata\\LINKDATA.BIN"    
+# "C:\\Users\\Malachi\\WorkDesktop\\Shin Sangokumusou 4 Special\\linkdata\\LINKDATA.IDX"
+# "C:\\Users\\Malachi\\WorkDesktop\\Shin Sangokumusou 4 Special\\STUFF\\LINKDATA.BIN_OUT\\"
